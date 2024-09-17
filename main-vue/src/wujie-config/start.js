1c547e6ccff80e935d57ac523cd0128d97c72cd4
RainTune <969723165@qq.com>
2024-09-17 18:05:28 +0800

    feat: demo修改

--- main-vue/src/wujie-config/start.js
+++ main-vue/src/wujie-config/start.js
@@ -17,7 +17,7 @@ export const registerApp = (Vue, router) => {
    * 此处修正 iframe 的 src，是防止github pages csp报错
    * 因为默认是只有 host+port，没有携带路径
    */
-  const attrs = isProduction ? { src: hostMap("//localhost:8080/") } : {};
+  const attrs = isProduction ? { src: hostMap("//localhost:8000/") } : {};
   setupApp({
     name: "vite",
     url: hostMap("//localhost:5173/"),
